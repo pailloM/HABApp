154e21a303cd5f2e637f73ae28935dafe5da1ebf
Paillomams <paillomams@gmx.com>
2021-06-10 00:25:13 -0400

    initial connection to homeassistant

---
+++ src/HABApp/home-assistant/errors.py
@@ -0,0 +1,60 @@
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Connection errors
+# ----------------------------------------------------------------------------------------------------------------------
+class HomeassistantConnectionNotSetUpError(Exception):
+    pass
+
+
+class HomeassistantDisconnectedError(Exception):
+    pass
+
+
+class HomeassistantNotReadyYet(Exception):
+    pass
+
+
+class ExpectedSuccessFromHomeassistant(Exception):
+    pass
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# OpenHAB errors
+# ----------------------------------------------------------------------------------------------------------------------
+class ItemNotFoundError(Exception):
+
+    @classmethod
+    def from_name(cls, name: str):
+        return cls(f'Item "{name}" not found!')
+
+
+class ItemNotEditableError(Exception):
+
+    @classmethod
+    def from_name(cls, name: str):
+        return cls(f'Item "{name}" is not editable!')
+
+
+class ThingNotFoundError(Exception):
+
+    @classmethod
+    def from_uid(cls, uid: str):
+        return cls(f'Thing "{uid}" not found!')
+
+
+class ThingNotEditableError(Exception):
+
+    @classmethod
+    def from_uid(cls, uid: str):
+        return cls(f'Thing "{uid}" is not editable!')
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# RestAPI Errors
+# ----------------------------------------------------------------------------------------------------------------------
+class MetadataNotEditableError(Exception):
+
+    @classmethod
+    def create_text(cls, item: str, namespace: str):
+        return cls(f'Metadata {namespace} for {item} is not editable!')

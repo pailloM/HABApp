fdfe24aae8aa75c88b270ab83e268206fdc999ad
Paillomams <paillomams@gmx.com>
2021-06-14 00:00:03 -0400

    debug

--- src/HABApp/homeassistant/interface.py
+++ src/HABApp/homeassistant/interface.py
@@ -1,6 +1,5 @@
 from HABApp.homeassistant.connection_handler.func_sync import \
     post_update, send_command, \
-    get_item, item_exists, remove_item, create_item, \
-    get_thing, get_persistence_data,\
+    item_exists, remove_item, create_item, \
+    get_persistence_data,\
     remove_metadata, set_metadata, \
-    get_channel_link, remove_channel_link, channel_link_exists, create_channel_link

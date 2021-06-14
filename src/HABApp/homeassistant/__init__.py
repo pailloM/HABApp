b5a18101243552c18df9b8c355942f28fb332655
Paillomams <paillomams@gmx.com>
2021-06-14 00:05:28 -0400

    debug

--- src/HABApp/homeassistant/__init__.py
+++ src/HABApp/homeassistant/__init__.py
@@ -6,4 +6,4 @@ import HABApp.homeassistant.interface_async
 import HABApp.homeassistant.interface
 
 # items use the interface for the convenience functions
-import HABApp.homeassistant.items
+# import HABApp.homeassistant.items

3f219b19e77fadb19de654b748471da34ef9f25e
Paillomams <paillomams@gmx.com>
2021-06-14 00:04:08 -0400

    debug

--- src/HABApp/homeassistant/interface.py
+++ src/HABApp/homeassistant/interface.py
@@ -1,5 +1,4 @@
 from HABApp.homeassistant.connection_handler.func_sync import \
     post_update, send_command, \
     item_exists, remove_item, create_item, \
-    get_persistence_data,\
     remove_metadata, set_metadata \

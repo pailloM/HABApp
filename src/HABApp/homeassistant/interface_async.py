5fd0468ffb4a5f5693f4f9c48a10726b9a5e1e0b
Paillomams <paillomams@gmx.com>
2021-06-14 00:01:40 -0400

    debug

--- src/HABApp/homeassistant/interface_async.py
+++ src/HABApp/homeassistant/interface_async.py
@@ -3,4 +3,4 @@ from HABApp.homeassistant.connection_handler.func_async import \
     async_get_items, async_get_item, async_item_exists, async_remove_item, async_create_item, \
     async_get_things, async_get_thing, async_set_thing_cfg, \
     async_remove_metadata, async_set_metadata, \
-    async_get_persistence_data,\
+    async_get_persistence_data \
